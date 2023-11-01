b3f3aa7a172cf88acec68f235392a7d84a1d2b29
Alessandro Pisu <alessandro96.pisu@gmail.com>
2023-11-01 08:38:02 +0000

    Margin per order

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -10,3 +10,25 @@ s.revenue - ROUND(s.quantity*p.purchase_price,2) AS margin
 FROM {{ref("stg_raw__sales")}} s
 LEFT JOIN {{ref("stg_raw__product")}} p 
 USING (products_id)
+
+SELECT
+	orders_id,
+	date_date,
+	ROUND(SUM(revenue),2) as revenue,
+	ROUND(SUM(quantity),2) as quantity,
+	ROUND(SUM(purchase_cost),2) as purchase_cost,
+	ROUND(SUM(margin),2) as margin
+FROM {{ ref("int_sales_margin") }}
+GROUP BY orders_id,date_date
+ORDER BY orders_id DESC
+
+SELECT
+	orders_id,
+	max(date_date) as date_date,
+	ROUND(SUM(revenue),2) as revenue,
+	ROUND(SUM(quantity),2) as quantity,
+	ROUND(SUM(purchase_cost),2) as purchase_cost,
+	ROUND(SUM(margin),2) as margin
+FROM {{ ref("int_sales_margin") }}
+GROUP BY orders_id
+ORDER BY orders_id DESC
